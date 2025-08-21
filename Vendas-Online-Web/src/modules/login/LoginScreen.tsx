357aa4e3796a0de7eabc16aa0b6b4ddda3df26d4
Leandro Mateus <leandro.mateus@techfive.local>
2025-08-21 16:01:13 -0300

    feat: creat module login

---
+++ Vendas-Online-Web/src/modules/login/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import { BackgroundImage } from "./components/screens/styles/loginScreen.styles";
+
+
+const LoginScreen = () => {
+    return (<div>
+        <BackgroundImage src='./background.png'/>
+    </div>);
+};
+
+export default LoginScreen;
